7a240f5dbaa630e21048d977da4002caa0d13db6
hlus <kyshbogdan@gmail.com>
2018-05-28 16:56:49 +0300

    add comments for TriangulatedPolygon class

--- src/sample/TriangulatedPolygon.java
+++ src/sample/TriangulatedPolygon.java
@@ -4,31 +4,77 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class describe triangulation information
+ * and get access for node of triangulation
+ *
+ * @author hlus
+ * @version 2.0
+ */
 public class TriangulatedPolygon implements Serializable {
 
-    private CostCell rootNode;
-    private int n;
+    private CostCell rootNode;  // root node of triangulation tree
+    private int n;              // vertex count
 
+    /**
+     * Simple getter for get access to root
+     * of triangulation tree
+     *
+     * @return root node of triangulation
+     */
     public CostCell getRootNode() {
         return rootNode;
     }
 
+    /**
+     * Simple getter for get access
+     * to count of vertex
+     *
+     * @return count of polygon vertex
+     */
     public int getN() {
         return n;
     }
 
+    /**
+     * Simple getter for get access
+     * to count of triangles
+     * after triangulation
+     *
+     * @return count of triangles
+     */
     public int getTrianglesCount() {
         return n - 2;
     }
 
+    /**
+     * Simple getter for get access
+     * to count of diagonals
+     * after triangulation
+     *
+     * @return count of diagonals
+     */
     public int getDiaglonalesCount() {
         return n - 3;
     }
 
+    /**
+     * This method get access for
+     * cost value (min sum of diagonals),
+     * after triangulation
+     *
+     * @return cost value after triangulation
+     */
     public double getCostSum() {
         return this.rootNode.getCost() - this.rootNode.getSeg().getCost();
     }
 
+    /**
+     * Simple constructor where build
+     * optimal triangulation tree
+     * and calculate minimal cost
+     * @param polygon polygon for triangulation
+     */
     public TriangulatedPolygon(Polygon polygon) {
         this.n = polygon.getPoints().size();
         CostCell[][] table = new CostCell[n][n];
