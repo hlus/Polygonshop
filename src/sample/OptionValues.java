be2ea63b3044b8280f1d63613ef5da4f4dae97f4
hlus <kyshbogdan@gmail.com>
2018-06-03 23:59:12 +0300

    add comments OptionValues class

--- src/sample/OptionValues.java
+++ src/sample/OptionValues.java
@@ -9,22 +9,26 @@ import java.util.List;
  * Class which describe option values
  *
  * @author hlus
- * @version 2.0
+ * @version 2.1
  */
 public class OptionValues implements Serializable {
 
-    public boolean showDescription;
-    public boolean showTree;
-    public double pointRadius;
-    public double lineWidth;
-    public double treeLineWidth;
-    public Color polygonBackground;
-    public Color vertexColor;
-    public Color leafColor;
-    public Color nodeColor;
-    public Color simpleLineColor;
-    public Color diagonalColor;
+    public boolean showDescription; // show or not (vertex name, diagonal descriptions)
+    public boolean showTree;        // show lines of triangulation tree
+    public double pointRadius;      // radius of vertex
+    public double lineWidth;        // width simple line
+    public double treeLineWidth;    // width triangulation tree line
+    public Color polygonBackground; // color of canvas background
+    public Color vertexColor;       // vertex color
+    public Color leafColor;         // leaf color (triangulation tree)
+    public Color nodeColor;         // node color (triangulation tree)
+    public Color simpleLineColor;   // color of simple line
+    public Color diagonalColor;     // diagonal color (after triangulation)
 
+    /**
+     * Getter build list of options
+     * @return list of options
+     */
     public List<Object> getOptions() {
         return Arrays.asList(
                 showDescription,
@@ -41,30 +45,57 @@ public class OptionValues implements Serializable {
         );
     }
 
+    /**
+     * Getter get polygon background color (convert in javafx color)
+     * @return polygonBackground
+     */
     public javafx.scene.paint.Color getPolygonBackground() {
         return ColorUtil.awtToFx(polygonBackground);
     }
 
+    /**
+     * Getter get vertex color (convert in javafx color)
+     * @return vertexColor
+     */
     public javafx.scene.paint.Color getVertexColor() {
         return ColorUtil.awtToFx(vertexColor);
     }
 
+    /**
+     * Getter get polygon leaf color (convert in javafx color)
+     * @return leafColor
+     */
     public javafx.scene.paint.Color getLeafColor() {
         return ColorUtil.awtToFx(leafColor);
     }
 
+    /**
+     * Getter get polygon node color (convert in javafx color)
+     * @return nodeColor
+     */
     public javafx.scene.paint.Color getNodeColor() {
         return ColorUtil.awtToFx(nodeColor);
     }
 
+    /**
+     * Getter get polygon line color (convert in javafx color)
+     * @return simpleLineColor
+     */
     public javafx.scene.paint.Color getSimpleLineColor() {
         return ColorUtil.awtToFx(simpleLineColor);
     }
 
+    /**
+     * Getter get polygon diagonal color (convert in javafx color)
+     * @return diagonalColor
+     */
     public javafx.scene.paint.Color getDiagonalColor() {
         return ColorUtil.awtToFx(diagonalColor);
     }
 
+    /**
+     * Constructor for OptionValues class
+     */
     public OptionValues() {
         this(Arrays.asList(
                 true,
@@ -81,10 +112,18 @@ public class OptionValues implements Serializable {
         ));
     }
 
+    /**
+     * Constructor for OptionValues class
+     * @param optionValues copy object
+     */
     public OptionValues(OptionValues optionValues) {
         this(optionValues.getOptions());
     }
 
+    /**
+     * Constructor for OptionValues class
+     * @param values values for initialize the new object
+     */
     public OptionValues(List<Object> values) {
         this.showDescription = (boolean) values.get(0);
         this.showTree = (boolean) values.get(1);
