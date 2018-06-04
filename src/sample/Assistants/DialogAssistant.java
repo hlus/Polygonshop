7eeac73c585d8b147013c42e5ea035eee168596d
hlus <kyshbogdan@gmail.com>
2018-06-04 17:04:07 +0300

    DialogAssistant code refactoring

--- src/sample/Assistants/DialogAssistant.java
+++ src/sample/Assistants/DialogAssistant.java
@@ -8,11 +8,14 @@ import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+import javafx.util.Pair;
 import sample.Controller;
 import sample.Model.TriangulatedPolygon;
 import sample.Model.UIPolygon;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * This class just generate dialogs
@@ -94,6 +97,39 @@ public class DialogAssistant {
     }
 
     /**
+     * Generic method which create dialog
+     * with 2 column of the fields
+     *
+     * @param style  String variable which describe style (.css)
+     * @param title  Dialog title
+     * @param fields list of the pairs Key - text of label, Value - some Control
+     * @param <T>    return data type
+     * @return dialog with 2 column of the fields
+     */
+    private static <T> Dialog<T> getFieldsDialog(String style, String title, List<Pair<String, Control>> fields) {
+        Dialog<T> dialog = new Dialog<>();
+        dialog.setTitle(title);
+
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(20, 150, 10, 10));
+
+        for (int i = 0; i < fields.size(); i++) {
+            grid.add(new Label(fields.get(i).getKey()), 0, i);
+            grid.add(fields.get(i).getValue(), 1, i);
+        }
+
+        dialog.getDialogPane().setContent(grid);
+        Platform.runLater(fields.get(0).getValue()::requestFocus);
+
+        dialog.getDialogPane().getStylesheets().add(style);
+        dialog.getDialogPane().getStyleClass().add("main");
+
+        return dialog;
+    }
+
+    /**
      * This static method which generate dialog
      * where is ask some options for create new 'polygon file'
      *
@@ -106,40 +142,18 @@ public class DialogAssistant {
      * @see Controller#style
      */
     public static Dialog<String[]> getCreateNewDialog(String style) {
-        // Create the custom dialog.
-        Dialog<String[]> dialog = new Dialog<>();
-        dialog.setTitle("Create new file");
-
-        // Set the button types.
-        ButtonType createButtonType = new ButtonType("Create", ButtonBar.ButtonData.OK_DONE);
-        dialog.getDialogPane().getButtonTypes().addAll(createButtonType, ButtonType.CANCEL);
-
-        // Create the username and password labels and fields.
-        GridPane grid = new GridPane();
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(20, 150, 10, 10));
-
         JFXTextField fileName = new JFXTextField("Untitled");
         JFXTextField canvasWidth = new JFXTextField("800");
         JFXTextField canvasHeight = new JFXTextField("600");
+        List<Pair<String, Control>> fields = new ArrayList<>(Arrays.asList(
+                new Pair<>("Name", fileName),
+                new Pair<>("Width:", canvasWidth),
+                new Pair<>("Height:", canvasHeight)
+        ));
 
-        grid.add(new Label("Name:"), 0, 0);
-        grid.add(fileName, 1, 0);
-
-        grid.add(new Label("Width:"), 0, 1);
-        grid.add(canvasWidth, 1, 1);
-
-        grid.add(new Label("Height:"), 0, 2);
-        grid.add(canvasHeight, 1, 2);
-
-        dialog.getDialogPane().setContent(grid);
-
-        // Request focus on the filename field by default.
-        Platform.runLater(fileName::requestFocus);
-
-        dialog.getDialogPane().getStylesheets().add(style);
-        dialog.getDialogPane().getStyleClass().add("main");
+        Dialog<String[]> dialog = getFieldsDialog(style, "Create new ", fields);
+        ButtonType createButtonType = new ButtonType("Create", ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().addAll(createButtonType, ButtonType.CANCEL);
 
         dialog.setResultConverter(dialogButton -> dialogButton == createButtonType ?
                 new String[]{fileName.getText(), canvasWidth.getText(), canvasHeight.getText()} :
@@ -156,14 +170,6 @@ public class DialogAssistant {
      * @return options dialog which was generated
      */
     public static Dialog<OptionValues> getOptionsDialog(String style, OptionValues values) {
-        Dialog<OptionValues> dialog = new Dialog<>();
-        dialog.setTitle("Options");
-        dialog.getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
-
-        GridPane grid = new GridPane();
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(20, 150, 10, 10));
 
         JFXCheckBox showDescription = new JFXCheckBox();
         showDescription.setSelected(values.showDescription);
@@ -178,46 +184,22 @@ public class DialogAssistant {
         JFXColorPicker nodeColor = new JFXColorPicker(values.getNodeColor());
         JFXColorPicker simpleLineColor = new JFXColorPicker(values.getSimpleLineColor());
         JFXColorPicker diagonalColor = new JFXColorPicker(values.getDiagonalColor());
+        List<Pair<String, Control>> fields = new ArrayList<>(Arrays.asList(
+                new Pair<>("Show description", showDescription),
+                new Pair<>("Show tree", showTree),
+                new Pair<>("Point radius:", pointRadius),
+                new Pair<>("Line width:", lineWidth),
+                new Pair<>("Tree line width:", treeLineWidth),
+                new Pair<>("Polygon background: ", background),
+                new Pair<>("Vertex color: ", vertexColor),
+                new Pair<>("Leaf color: ", leafColor),
+                new Pair<>("Node color: ", nodeColor),
+                new Pair<>("Line color: ", simpleLineColor),
+                new Pair<>("Diagonal color: ", diagonalColor)
+        ));
 
-
-        grid.add(new Label("Show description"), 0, 0);
-        grid.add(showDescription, 1, 0);
-
-        grid.add(new Label("Show tree"), 0, 1);
-        grid.add(showTree, 1, 1);
-
-        grid.add(new Label("Point radius:"), 0, 2);
-        grid.add(pointRadius, 1, 2);
-
-        grid.add(new Label("Line width:"), 0, 3);
-        grid.add(lineWidth, 1, 3);
-
-        grid.add(new Label("Tree line width:"), 0, 4);
-        grid.add(treeLineWidth, 1, 4);
-
-        grid.add(new Label("Polygon background: "), 0, 5);
-        grid.add(background, 1, 5);
-
-        grid.add(new Label("Vertex color: "), 0, 6);
-        grid.add(vertexColor, 1, 6);
-
-        grid.add(new Label("Leaf color: "), 0, 7);
-        grid.add(leafColor, 1, 7);
-
-        grid.add(new Label("Node color: "), 0, 8);
-        grid.add(nodeColor, 1, 8);
-
-        grid.add(new Label("Line color: "), 0, 9);
-        grid.add(simpleLineColor, 1, 9);
-
-        grid.add(new Label("Diagonal color: "), 0, 10);
-        grid.add(diagonalColor, 1, 10);
-
-        dialog.getDialogPane().setContent(grid);
-        Platform.runLater(showDescription::requestFocus);
-
-        dialog.getDialogPane().getStylesheets().add(style);
-        dialog.getDialogPane().getStyleClass().add("main");
+        Dialog<OptionValues> dialog = getFieldsDialog(style, "Options", fields);
+        dialog.getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
 
         dialog.setResultConverter(dialogButton -> {
             OptionValues res = null;
